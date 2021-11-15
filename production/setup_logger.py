c1ac08318b8e1e63df9382cc8427bff8a6b58182
BrandonCay <brandoncay525@gmail.com>
2021-11-15 14:12:45 -0500

    changed setup_logger

--- production/setup_logger.py
+++ production/setup_logger.py
@@ -1,9 +1,10 @@
 import logging
 import os
+from data.absolutePathToLog import absolutePathToLog
 
 def setup_logger(logger_name, log_file, level=logging.DEBUG):
     log_file = os.path.basename(log_file)
-    absolutePathOfFile = rf'C:\Users\Brandon\Documents\College\Fall2021\CS4800-CS-Seminar\researchProj\colabCode\normalizeData\logs\{log_file}'
+    absolutePathOfFile = rf'{absolutePathToLog}\{log_file}'
     print("ABS: " + absolutePathOfFile)
     log_file = absolutePathOfFile
     l = logging.getLogger(logger_name)
